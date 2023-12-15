9a6715e3effd2f6b114e1944df6cbdfb0e7dffb4
roshani2022 <rosh.gupta17@gmail.com>
2023-12-15 21:32:33 +0530

    Integrating it with firebase

--- src/Component/Expense/Expense.js
+++ src/Component/Expense/Expense.js
@@ -1,5 +1,5 @@
 
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import ExpenseForm from "./ExpneseForm";
 import ExpenseList from "./ExpenseList";
 
@@ -7,6 +7,31 @@ const Expense = () => {
    
     const [expenseList, setExpenseList] = useState([]);
     const [isExpense, setIsExpense] = useState(false);
+
+    let url =
+    "https://expense-tracker-c2f34-default-rtdb.firebaseio.com/expenses.json";
+
+    useEffect(() => {
+        const getData = async () => {
+          try {
+            const res = await fetch(url);
+    
+            if (res.ok) {
+              const data = await res.json();
+              // Update the state with the fetched data
+            //   setExpenseList(data ? Object.values(data) : []);
+            setExpenseList(data ? Object.values(data) : []);
+              setIsExpense(true);
+            } else {
+              console.log("Failed to fetch data:", res.status);
+            }
+          } catch (err) {
+            console.log("Error fetching data:", err);
+          }
+        };
+    
+        getData();
+      }, []);
 
     const addExpenseHandler = (amount,detail,category) => {
         const newExpense = {
--- src/Component/Expense/ExpneseForm.js
+++ src/Component/Expense/ExpneseForm.js
@@ -1,57 +1,97 @@
-import React,{useState} from "react";
-import { Card, Form,Button} from "react-bootstrap";
+import React, {useState } from "react";
+import { Card, Form, Button } from "react-bootstrap";
 
 const ExpenseForm = (props) => {
+  const [amount, setAmount] = useState("");
+  const [detail, setDetail] = useState("");
+  const [category, setCategory] = useState("Food");
 
-    const [amount,setAmount] = useState()
-    const [detail,setDetail] = useState()
-    const [category,setCategory] = useState()
+  
 
-    const amountHandler = (event) => {
-        setAmount(event.target.value)
+  let url =
+    "https://expense-tracker-c2f34-default-rtdb.firebaseio.com/expenses.json";
+
+  const amountHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const detailHandler = (event) => {
+    setDetail(event.target.value);
+  };
+  const categoryHandler = (event) => {
+    setCategory(event.target.value);
+  };
+
+  
+
+  const addExpenseHandler = async (event) => {
+    event.preventDefault();
+
+    let expenses = { amount, detail, category };
+
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(expenses),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+
+        setAmount("");
+        setDetail("");
+        setCategory("Food");
+      } else {
+        console.log(res.error);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    const detailHandler = (event)=> {
-        setDetail(event.target.value)
-    }
-    const categoryHandler = (event) => {
-        setCategory(event.target.value)
-    }
 
-    const addExpenseHandler = (event) => {
-       event.preventDefault()
-
-       props.onAdd(amount,detail,category)
-      setAmount("")
-      setDetail("")
-      setCategory("")
-
-    }
-
-
+    props.onAdd(amount, detail, category);
+  };
 
   return (
-    <Card className="mt-5 mx-auto" style={{ width: "400px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", fontWeight:"bold"}}>
+    <Card
+      className="mt-5 mx-auto"
+      style={{
+        width: "400px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        fontWeight: "bold",
+      }}
+    >
       <Form onSubmit={addExpenseHandler}>
         <Form.Group className="mb-3" controlId="formGroupNumber">
           <Form.Label>Expense Amount</Form.Label>
-          <Form.Control type="number" placeholder="Amount" onChange={amountHandler} value={amount} />
+          <Form.Control
+            type="number"
+            placeholder="Amount"
+            onChange={amountHandler}
+            value={amount}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupText">
           <Form.Label>Expense Detail</Form.Label>
-          <Form.Control type="text" placeholder="ExpenseDetail" onChange={detailHandler} value={detail} />
+          <Form.Control
+            type="text"
+            placeholder="ExpenseDetail"
+            onChange={detailHandler}
+            value={detail}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formGridCategory" >
+        <Form.Group className="mb-3" controlId="formGridCategory">
           <Form.Label>Expense Category</Form.Label>
-          <Form.Select defaultValue="Choose Category..." onChange={categoryHandler} value={category}>
+          <Form.Select onChange={categoryHandler} value={category}>
             <option>Food</option>
             <option>Grocery</option>
             <option>Travel</option>
           </Form.Select>
         </Form.Group>
         <div className="d-flex justify-content-center mt-3 mb-3">
-        <Button type='submit'>Add Expense</Button>
+          <Button type="submit">Add Expense</Button>
         </div>
-       
       </Form>
     </Card>
   );
